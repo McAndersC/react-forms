254139eb85cd04418dcfcd365a78cc915e6e96e5
Anders Christensen <anders@medieskolerne.dk>
2023-06-06 09:19:20 +0200

    forms

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,78 +1,99 @@
-'use client';
+"use client";
 import { useForm } from "react-hook-form";
-import styles from './page.module.css';
+import styles from "./page.module.css";
 import ContactForm from "@/components/contactForm";
 
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object().shape({
-    name: Yup.string().required(),
+  name: Yup.string().required(),
+  message: Yup.string().required(),
+  email: Yup.string().email("TAG DIG SAMMEN!").required("DEN SKAL VÆRE DER!"),
+  age: Yup.number()
+    .typeError("DU SKAL SKRIVE ET TAL")
+    .required("ET TAL SKAL VÆRE DER"),
 });
-  
+
+
+
 const Contact = () => {
+  const onSubmit = (data) => {
+    const { name, email, message, age, color, date } = data;
 
-    const onSubmit = (data) => {
+    let contactObject = {
+      name: name,
+      email: email,
+      message: message,
+      age: age,
+      color: color,
+      date: date,
+    };
+    console.log("contactObject", contactObject);
+  };
 
-        const { name, email, message, age } = data;
-  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "Anders",
+      email: "anders@medieskolerne.dk",
+      color: "#973B3B",
+      message: "En forudfyldt besked",
+      age: 12,
+    },
+    resolver: yupResolver(validationSchema),
+  });
 
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <fieldset>
+          <legend>Create Contact</legend>
 
-        let contactObject = {
-            'name' : name,
-            'email' : email,
-            'message' : message,
-            'age' : age
-        }
-        console.log('contactObject', contactObject);
+          <label>
+            Name
+            <input {...register("name")} placeholder="indtast navn" />
+            {errors.name?.message && <p>{errors.name?.message}</p>}
+          </label>
 
-    }
+          <label>
+            Email
+            <input {...register("email")} />
+            {errors.email?.message && <p>{errors.email?.message}</p>}
+          </label>
 
-    const { register, handleSubmit, formState: { errors }, } = useForm({
-        defaultValues: {
-            name: 'Indtast navn',
-        },
-        resolver: yupResolver(validationSchema)
-    });
+          <label>
+            Message!
+            <input {...register("message")} />
+            {errors.message?.message && <p>{errors.message?.message}</p>}
+          </label>
 
-    return (
+          <label>
+            Age!
+            <input {...register("age")} />
+            {errors.age?.message && (
+              <p style={{ color: "red" }}>{errors.age?.message}</p>
+            )}
+          </label>
 
-        <div>
-            <h1>Contact Page</h1>
-            <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-                <fieldset>
-                <legend>Create Contact</legend>
+          <label>
+            Date
+            <input {...register("date")} type="date" />
+            {errors.date?.message && (
+              <p style={{ color: "red" }}>{errors.date?.message}</p>
+            )}
+          </label>
+        </fieldset>
 
-                <label>Name
-                    <input {...register('name')} />
-                    {errors.name?.message && <p>{errors.name?.message}</p>}
-                </label>
-
-                <label>Email
-                    <input {...register('email')} />
-                </label>
-
-                <label>Message!
-                    <input {...register('message')} />
-                </label>
-
-                <label>Age!
-                    <input {...register('age')} type="number"/>
-                </label>
-
-                </fieldset>
-
-                <button type="submit">Add Contact</button>
-
-            </form>
-
-            <h1>Contact Component</h1>
-            <ContactForm></ContactForm>
-        </div>
-
-
-    )
-
-}
+        <button type="submit">Add Contact</button>
+      </form>
+           
+    </div>
+  );
+};
 
 export default Contact;
