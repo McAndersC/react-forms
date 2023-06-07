bb6604ef606a32f7526160eda5adb077f4229aa7
Anders Christensen <anders@medieskolerne.dk>
2023-06-07 09:00:22 +0200

    db

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
 
+{console.log(process.env.MONGODB_URI)}
+
 export default function Home() {
   return (
     <main className={styles.main}>
